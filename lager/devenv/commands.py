c476a35c57517f86ac0a4e609d7bd0d1c9b600a8
Evan Haas <evan@lagerdata.com>
2020-06-25 17:20:28 -0700

    command saving and running

--- lager/devenv/commands.py
+++ lager/devenv/commands.py
@@ -29,7 +29,7 @@ def _devenv_section(name):
 def figure_out_devenv(name):
     config = read_config_file()
     if name is None:
-        names = _get_devenv_names()
+        names = _get_devenv_names(config, target_source_dir=os.getcwd())
         if not names:
             raise click.UsageError(
                 'No development environments defined',
@@ -40,13 +40,11 @@ def figure_out_devenv(name):
             )
         name = names[0]
 
-    config = read_config_file()
     section = _devenv_section(name)
     if not config.has_section(section):
         raise click.UsageError(
             f'Development environment {name} not defined',
         )
-
     return config, config[section]
 
 def _get_default_name():
@@ -77,14 +75,13 @@ def create(name, image, source_dir, mount_dir, shell):
     config.set(section, 'shell', shell)
     write_config_file(config)
 
-def _get_devenv_names():
-    config = read_config_file()
-    return [
-        section.split('.', 1)[1]
-        for section in config.sections()
-        if section.startswith('DEVENV.')
-    ]
-
+def _get_devenv_names(config, target_source_dir=None):
+    all_sections = [s for s in config.sections() if s.startswith('DEVENV.')]
+    if target_source_dir:
+        all_sections = [
+            s for s in all_sections if config.get(s, 'source_dir') == target_source_dir
+        ]
+    return [s.split('.', 1)[1] for s in all_sections]
 
 
 @devenv.command(name='list')
@@ -92,15 +89,21 @@ def list_():
     """
         Show development environment names
     """
-    names = _get_devenv_names()
+    config = read_config_file()
+    names = _get_devenv_names(config)
     if not names:
         click.echo('No development environments defined!')
     for name in names:
+        section = _devenv_section(name)
         click.echo(name)
+        click.echo(f'\timage: {config.get(section, "image")}')
+        click.echo(f'\tsource dir: {config.get(section, "source_dir")}')
+        click.echo(f'\tmount dir: {config.get(section, "mount_dir")}')
+        click.echo(f'\tshell: {config.get(section, "shell")}')
 
 
 @devenv.command()
-@click.option('--name')
+@click.option('--name', help='Devenv for which to launch an interactive terminal')
 def terminal(name):
     """
         Start an interactive terminal for a docker image
@@ -124,10 +127,15 @@ def terminal(name):
 @devenv.command()
 @click.pass_context
 @click.argument('cmd_name', required=False)
-@click.option('--name', required=False)
-@click.option('--command')
-@click.option('--save-as', default=None)
+@click.option('--name', required=False, help='Devenv name in which to run the command', metavar='<devenv>')
+@click.option('--command', help='Raw commandline to run in docker container', metavar='<cmdline>')
+@click.option('--save-as', default=None, help='Alias under which to save command specified with --command', metavar='<alias>')
 def run(ctx, cmd_name, name, command, save_as):
+    """
+        Run CMD_NAME in a docker container. CMD_NAME is a named command which was previously saved using `--save-as`.
+        If CMD_NAME is not provided, run the command specified by --command. If --save-as is also provided,
+        save the command under that name for later use with CMD_NAME
+    """
     config, section = figure_out_devenv(name)
 
     if not cmd_name and not command:

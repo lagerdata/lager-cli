113f29239349bfd69f32f8073b05bcfeb1fe95b3
David Cohen <david@fictivekin.com>
2020-07-01 16:31:38 -0400

    Configure setup.py for deployment

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # lager-cli
 Lager Command Line Interface
+
+## Development
+
+### Installation for development purposes
+
+```
+python3 -m virtualenv $(< .venv)
+source $(< .venv)/bin/activate
+pip install --editable .
+```
+
+With the virtualenv sourced, the `lager` executable should be available.
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 import os
 import setuptools
 
-import lager
+from lager import __version__ as lager_version
 
 def readme():
     path = os.path.dirname(__file__)
@@ -26,7 +26,7 @@ classifiers = [
 if __name__ == "__main__":
     setuptools.setup(
         name=name,
-        version=lager.__version__,
+        version=lager_version,
         description=description,
         long_description=readme(),
         classifiers=classifiers,
@@ -36,13 +36,37 @@ if __name__ == "__main__":
         maintainer=author,
         maintainer_email=email,
         license='MIT',
-        python_requires=">=3.5",
+        python_requires=">=3.6",
         packages=setuptools.find_packages(),
-        install_requires=[
-            'click',
-            'requests',
-        ],
-        scripts=[
-            'bin/lager',
-        ],
+        install_requires='''
+            async-generator == 1.10
+            bson == 0.5.10
+            certifi == 2020.4.5.1
+            chardet == 3.0.4
+            click == 7.1.2
+            colorama == 0.4.3
+            h11 == 0.9.0
+            idna == 2.9
+            ipaddress == 1.0.23
+            multidict == 4.7.6
+            outcome == 1.0.1
+            python-dateutil == 2.8.1
+            requests == 2.23.0
+            requests-toolbelt == 0.9.1
+            six == 1.15.0
+            sniffio == 1.1.0
+            sortedcontainers == 2.2.2
+            texttable == 1.6.2
+            trio == 0.16.0
+            trio-websocket == 0.8.0
+            urllib3 == 1.25.9
+            wsaccel == 0.6.2
+            wsproto == 0.14.1
+            yarl == 1.4.2
+        ''',
+        entry_points={
+            'console_scripts': [
+                'lager=lager.cli:cli',
+            ],
+        }
     )

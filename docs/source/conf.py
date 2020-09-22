b34a6c994760c6544543ba06d082d6e5cc428656
Jared McKnight <jmcknight@archertech.ca>
2020-09-22 14:51:46 -0400

    adds favicon

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -47,6 +47,8 @@ smartquotes = False
 #
 html_theme = 'sphinx_rtd_theme'
 
+html_favicon = 'lager-favicon.png'
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".

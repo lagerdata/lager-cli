274aea9835dc8e8a4dd91aa63ba79bb36dee2914
David Cohen <david@fictivekin.com>
2020-09-25 16:01:56 -0400

    Additional wifi commands

--- lager_cli/wifi/commands.py
+++ lager_cli/wifi/commands.py
@@ -25,7 +25,37 @@ def _wifi():
 
 @_wifi.command()
 @click.pass_context
-@click.option('--gateway', required=False, help='ID of gateway to which DUT is connected')
+@click.option('--gateway', required=False, help='ID of gateway')
+def status(ctx, gateway):
+    """
+        Get the current WiFi Status of the gateway
+    """
+    if gateway is None:
+        gateway = get_default_gateway(ctx)
+
+    session = ctx.obj.session
+    resp = session.get_wifi_state(gateway)
+    click.echo(resp.content, nl=False)
+
+
+@_wifi.command()
+@click.pass_context
+@click.option('--gateway', required=False, help='ID of gateway')
+def access_points(ctx, gateway):
+    """
+        Get WiFi access points visible to the gateway
+    """
+    if gateway is None:
+        gateway = get_default_gateway(ctx)
+
+    session = ctx.obj.session
+    resp = session.get_wifi_access_points(gateway)
+    click.echo(resp.content, nl=False)
+
+
+@_wifi.command()
+@click.pass_context
+@click.option('--gateway', required=False, help='ID of gateway')
 @click.option('--ssid', required=True, help='SSID of the network to connect to')
 @click.option('--password', required=False, help='Password of the network to connect to', default='')
 def connect(ctx, gateway, ssid, password=''):
@@ -37,4 +67,20 @@ def connect(ctx, gateway, ssid, password=''):
 
     session = ctx.obj.session
     resp = session.connect_wifi(gateway, ssid, password)
+    click.echo(resp.content, nl=False)
+
+
+@_wifi.command()
+@click.pass_context
+@click.option('--gateway', required=False, help='ID of gateway')
+@click.option('--ssid', required=True, help='SSID of the network to delete')
+def delete_connection(ctx, gateway, ssid):
+    """
+        Delete the specified network from the gateway
+    """
+    if gateway is None:
+        gateway = get_default_gateway(ctx)
+
+    session = ctx.obj.session
+    resp = session.delete_wifi_connection(gateway, ssid)
     click.echo(resp.content, nl=False)

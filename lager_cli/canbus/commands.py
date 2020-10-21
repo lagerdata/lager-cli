188788d352a5f1f1023e5a0bb2546303b4d58a0f
Evan Haas <evan@lagerdata.com>
2020-10-21 12:38:18 -0700

    use latest image

--- lager_cli/canbus/commands.py
+++ lager_cli/canbus/commands.py
@@ -3,23 +3,22 @@
 
     Commands for canbus
 """
-import itertools
 import click
 from ..context import get_default_gateway
 
 @click.group(name='canbus')
 def canbus():
     """
-        Lager canbus commands
+        Lager CAN Bus commands
     """
     pass
 @canbus.command()
 @click.pass_context
 @click.option('--gateway', required=False, help='ID of gateway to which DUT is connected')
-@click.option('--bitrate', required=True, type=click.INT, help='bus bitrate')
+@click.option('--bitrate', required=True, type=click.INT, help='bus bitrate e.g. 500000, 250000, etc')
 def up(ctx, gateway, bitrate):
     """
-        Bring up the CAN bus
+        Bring up the CAN Bus at the specified bitrate
     """
     if gateway is None:
         gateway = get_default_gateway(ctx)
